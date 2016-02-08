430385fb4737544a9abeda456e3a4dd854dd613e
bradmartin <bradmartin0905@gmail.com>
2016-02-08 09:39:52 -0600

    fix missing .js files

--- video-source/video-source-common.js
+++ video-source/video-source-common.js
@@ -1,39 +1,37 @@
-import utils = require("utils/utils");
-
+var utils = require("utils/utils");
 // This is used for definition purposes only, it does not generate JavaScript for it.
-import definition = require("./video-source");
-
-export function fromResource(name: string): definition.VideoSource {
+var definition = require("./video-source");
+function fromResource(name) {
     var video = new definition.VideoSource();
     return video.loadFromResource(name) ? video : null;
 }
-
-export function fromFile(path: string): definition.VideoSource { 
+exports.fromResource = fromResource;
+function fromFile(path) {
     var video = new definition.VideoSource();
     return video.loadFromFile(path) ? video : null;
 }
-
-export function fromNativeSource(source: any): definition.VideoSource {
+exports.fromFile = fromFile;
+function fromNativeSource(source) {
     var video = new definition.VideoSource();
     return video.setNativeSource(source) ? video : null;
 }
-
-export function fromUrl(url: string): definition.VideoSource {
-    var video = new definition.VideoSource(); 
+exports.fromNativeSource = fromNativeSource;
+function fromUrl(url) {
+    var video = new definition.VideoSource();
     return video.loadFromUrl(url) ? video : null;
 }
-
-export function fromFileOrResource(path: string): definition.VideoSource {
+exports.fromUrl = fromUrl;
+function fromFileOrResource(path) {
     if (!isFileOrResourcePath(path)) {
         throw new Error("Path \"" + "\" is not a valid file or resource.");
     }
-
     if (path.indexOf(utils.RESOURCE_PREFIX) === 0) {
         return fromResource(path.substr(utils.RESOURCE_PREFIX.length));
     }
     return fromFile(path);
 }
-
-export function isFileOrResourcePath(path: string): boolean {
+exports.fromFileOrResource = fromFileOrResource;
+function isFileOrResourcePath(path) {
     return utils.isFileOrResourcePath(path);
 }
+exports.isFileOrResourcePath = isFileOrResourcePath;
--- video-source/video-source.android.js
+++ video-source/video-source.android.js
@@ -1,99 +1,80 @@
-﻿import types = require("utils/types");
-import definition = require("video-source");
-import common = require("./video-source-common");
-import * as utilsModule from "utils/utils";
-import * as fileSystemModule from "file-system";
-import * as enumsModule from "ui/enums";
-
+var types = require("utils/types");
+var common = require("./video-source-common");
 global.moduleMerge(common, exports);
-
-var utils: typeof utilsModule;
-function ensureUtils() { 
+var utils;
+function ensureUtils() {
     if (!utils) {
         utils = require("utils/utils");
     }
 }
-
-var fs: typeof fileSystemModule;
+var fs;
 function ensureFS() {
     if (!fs) {
         fs = require("file-system");
-    } 
+    }
 }
-
-var enums: typeof enumsModule;
+var enums;
 function ensureEnums() {
     if (!enums) {
         enums = require("ui/enums");
     }
 }
-
-export class VideoSource implements definition.VideoSource {
-    public android: android.widget.VideoView;
-    public ios: AVPlayer;
-
-    public loadFromResource(name: string): boolean {
+var VideoSource = (function () {
+    function VideoSource() {
+    }
+    VideoSource.prototype.loadFromResource = function (name) {
         this.android = null;
-
         ensureUtils();
-
         var res = utils.ad.getApplicationContext().getResources();
         if (res) {
             //var identifier: number = res.getIdentifier(name, 'drawable', utils.ad.getApplication().getPackageName());
             //console.log('identifier: ' + identifier);
-
             var UrlPath = "android.resource://org.nativescript.videoPlayer/R.raw." + name;
             console.log('uri: ' + UrlPath);
-
             // var uri: string = android.net.Uri.parse("android.resource://" + utils.ad.getApplication().getPackageName() + "/R.raw." + name);
-
             this.android = UrlPath;
-
         }
-
         return this.android != null;
-    }
-
-    public loadFromUrl(url: string): boolean {
+    };
+    VideoSource.prototype.loadFromUrl = function (url) {
         this.android = null;
         this.android = url;
         return this.android != null;
-    }
-
-    public loadFromFile(path: string): boolean {
-
+    };
+    VideoSource.prototype.loadFromFile = function (path) {
         ensureFS();
-
         var fileName = types.isString(path) ? path.trim() : "";
         if (fileName.indexOf("~/") === 0) {
             fileName = fs.path.join(fs.knownFolders.currentApp().path, fileName.replace("~/", ""));
             console.log('fileName: ' + fileName);
         }
-
         this.android = fileName;
         return this.android != null;
-    }
-
-    public setNativeSource(source: any): boolean {
+    };
+    VideoSource.prototype.setNativeSource = function (source) {
         this.android = source;
         return source != null;
-    }
-
-
-    get height(): number {
-        if (this.android) {
-            return this.android.getHeight();
-        }
-
-        return NaN;
-    }
-
-    get width(): number {
-        if (this.android) {
-            return this.android.getWidth();
-        }
-
-        return NaN;
-    }
-
-}
+    };
+    Object.defineProperty(VideoSource.prototype, "height", {
+        get: function () {
+            if (this.android) {
+                return this.android.getHeight();
+            }
+            return NaN;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(VideoSource.prototype, "width", {
+        get: function () {
+            if (this.android) {
+                return this.android.getWidth();
+            }
+            return NaN;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return VideoSource;
+})();
+exports.VideoSource = VideoSource;
