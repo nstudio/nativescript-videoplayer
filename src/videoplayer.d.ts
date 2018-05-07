ef3ea295fda7065ecab1b78a47f83159aa2c6d53
Dave Coffin <dave@nimbit.com>
2018-05-07 11:56:47 -0400

    remove platform declarations as they are not needed and throw a typescript error.

--- src/videoplayer.d.ts
+++ src/videoplayer.d.ts
@@ -1,6 +1,3 @@
-/// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
-/// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" />
-
 import { View } from 'tns-core-modules/ui/core/view';
 
 export declare class Video extends View {
